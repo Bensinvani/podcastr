6110e718b086e50ae6238392ea8bf20285548a3d
Ben sinvani <Bensinvani7@gmail.com>
2025-04-27 00:49:05 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://delicate-reindeer-36.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
